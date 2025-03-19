92631fd3cd6f5542510d729936add1d59da952fd
Aleksandr Pasevin <pasevin@gmail.com>
2025-03-19 17:13:34 +0200

    chore(deps): update dependencies and remove duplicated setup files

--- packages/form-renderer/src/test/setup.ts
+++
@@ -1,9 +0,0 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
-
-import '@testing-library/jest-dom';
-
-// Automatically cleanup after each test
-afterEach(() => {
-  cleanup();
-});
