f4e37122a06115458cf1c97ac287a01256cb74dc
Aleksandr Pasevin <pasevin@gmail.com>
2025-06-26 18:34:01 +0300

    refactor(config): migrate library packages from Vite to tsup

--- packages/adapter-midnight/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { createLibraryConfig } from '../../vite.shared.config';
-
-export default createLibraryConfig({
-  packageDir: __dirname,
-  external: ['@midnight-ntwrk/dapp-connector-api', '@midnight-ntwrk/wallet-api'],
-});
--- packages/adapter-stellar/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { createLibraryConfig } from '../../vite.shared.config';
-
-export default createLibraryConfig({
-  packageDir: __dirname,
-  external: ['@stellar/stellar-sdk', '@stellar/freighter-api'],
-});
