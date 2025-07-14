6626a58b025614883be89632e4ec53a19195cd72
Aleksandr Pasevin <pasevin@gmail.com>
2025-07-14 12:17:48 +0300

    chore(release): sync versions

--- packages/core/src/export/versions.ts
+++ packages/core/src/export/versions.ts
@@ -7,12 +7,12 @@
 
 export const packageVersions = {
   '@openzeppelin/transaction-form-adapter-evm': '1.17.1',
-  '@openzeppelin/transaction-form-adapter-midnight': '0.3.0',
+  '@openzeppelin/transaction-form-adapter-midnight': '0.3.1',
   '@openzeppelin/transaction-form-adapter-solana': '0.3.0',
   '@openzeppelin/transaction-form-adapter-stellar': '0.3.0',
-  '@openzeppelin/transaction-form-react-core': '1.16.0',
-  '@openzeppelin/transaction-form-renderer': '1.17.0',
+  '@openzeppelin/transaction-form-react-core': '1.17.0',
+  '@openzeppelin/transaction-form-renderer': '1.17.1',
   '@openzeppelin/transaction-form-types': '1.17.0',
-  '@openzeppelin/transaction-form-ui': '1.17.0',
+  '@openzeppelin/transaction-form-ui': '1.18.0',
   '@openzeppelin/transaction-form-utils': '1.17.0',
 };
