08fb754ede2c28c31388541d34b415229cda9fd6
Aleksandr Pasevin <pasevin@gmail.com>
2025-07-17 16:57:43 +0300

    refactor(adapter-evm): integrate Etherscan V2 support for ABI loading

--- packages/adapter-evm/src/abi/index.ts
+++ packages/adapter-evm/src/abi/index.ts
@@ -1,3 +1,5 @@
 // Barrel file for abi module
 export * from './transformer';
-export * from './loader';
+export { loadAbiFromEtherscan } from './etherscan';
+export { loadAbiFromEtherscanV2, shouldUseV2Api } from './etherscan-v2';
+export { loadEvmContract } from './loader';
