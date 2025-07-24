fc9482e8d9f513ef683954754122ff1e19620006
Aleksandr Pasevin <pasevin@gmail.com>
2025-07-24 22:10:35 +0300

    fix(builder): make sidebar scrollable

--- packages/builder/src/components/Sidebar/AppSidebar/AppSidebar.tsx
+++ packages/builder/src/components/Sidebar/AppSidebar/AppSidebar.tsx
@@ -36,25 +36,31 @@ export default function AppSidebar({
         className={cn(
           // TODO: Replace hard-coded sidebar background with OpenZeppelin theme
           // Should use semantic token like 'bg-sidebar-background'
-          'fixed left-0 top-0 z-40 h-full w-[289px] bg-[rgba(245,245,245,0.31)] flex flex-col px-8 py-12',
+          'fixed left-0 top-0 z-40 h-full w-[289px] bg-[rgba(245,245,245,0.31)] flex flex-col',
           className
         )}
       >
-        <SidebarLogo />
+        {/* Fixed Header - Logo */}
+        <div className="flex-shrink-0 px-8 pt-12">
+          <SidebarLogo />
+        </div>
 
-        <div className="flex flex-col gap-12 w-full">
-          <MainActions
-            onCreateNew={onCreateNew}
-            onShowImportDialog={() => setShowImportDialog(true)}
-          />
+        {/* Scrollable Content Area */}
+        <div className="flex-1 overflow-y-auto px-8 pb-12">
+          <div className="flex flex-col gap-12 w-full">
+            <MainActions
+              onCreateNew={onCreateNew}
+              onShowImportDialog={() => setShowImportDialog(true)}
+            />
 
-          <OtherToolsSection />
+            <OtherToolsSection />
 
-          <ContractUIsSection
-            onLoadContractUI={onLoadContractUI}
-            onResetAfterDelete={onResetAfterDelete}
-            currentLoadedConfigurationId={currentLoadedConfigurationId}
-          />
+            <ContractUIsSection
+              onLoadContractUI={onLoadContractUI}
+              onResetAfterDelete={onResetAfterDelete}
+              currentLoadedConfigurationId={currentLoadedConfigurationId}
+            />
+          </div>
         </div>
       </div>
 
