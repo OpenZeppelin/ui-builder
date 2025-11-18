e3541f025971800b1c5fb40e1cd5554676f1350f
Aleksandr Pasevin <pasevin@gmail.com>
2025-11-18 17:15:51 +0200

    refactor(builder): export ExecutionConfig from execution module
    Define ExecutionConfig union type in execution/index.ts as the single source of truth for all execution configuration types.

--- packages/types/src/execution/index.ts
+++ packages/types/src/execution/index.ts
@@ -1,10 +1,10 @@
-export * from './eoa';
-export * from './multisig';
-export * from './relayer';
-
 import type { EoaExecutionConfig } from './eoa';
 import type { MultisigExecutionConfig } from './multisig';
 import type { RelayerExecutionConfig } from './relayer';
+
+export * from './eoa';
+export * from './multisig';
+export * from './relayer';
 
 /**
  * Execution configuration type (union of all execution methods)
