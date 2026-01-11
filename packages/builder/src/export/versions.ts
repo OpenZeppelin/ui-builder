752b9a0b10ae680f0c6e13840de8bf6f167f19c3
Aleksandr Pasevin <pasevin@gmail.com>
2026-01-11 19:22:54 +0300

    chore(deps): update @openzeppelin/ui-components to 1.0.4

--- packages/builder/src/export/versions.ts
+++ packages/builder/src/export/versions.ts
@@ -14,7 +14,7 @@ export const packageVersions = {
   '@openzeppelin/ui-renderer': '1.0.2',
   '@openzeppelin/ui-storage': '1.0.0',
   '@openzeppelin/ui-types': '1.1.0',
-  '@openzeppelin/ui-components': '1.0.3',
+  '@openzeppelin/ui-components': '1.0.4',
   '@openzeppelin/ui-utils': '1.0.0',
   '@openzeppelin/ui-styles': '1.0.0',
 };
