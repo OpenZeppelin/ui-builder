369cf5744ab775464235fa3ad13a41e54f8eeb6e
Aleksandr Pasevin <pasevin@gmail.com>
2026-01-21 13:53:42 +0300

    chore: update versions.ts and snapshots for ui-types 1.3.0

--- apps/builder/src/export/versions.ts
+++ apps/builder/src/export/versions.ts
@@ -13,7 +13,7 @@ export const packageVersions = {
   '@openzeppelin/ui-react': '1.1.0',
   '@openzeppelin/ui-renderer': '1.0.2',
   '@openzeppelin/ui-storage': '1.0.0',
-  '@openzeppelin/ui-types': '1.2.0',
+  '@openzeppelin/ui-types': '1.3.0',
   '@openzeppelin/ui-components': '1.1.0',
   '@openzeppelin/ui-utils': '1.1.0',
   '@openzeppelin/ui-styles': '1.0.0',
