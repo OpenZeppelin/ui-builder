b1b313533b5de261a105acd7fa31aecde0853b0a
Aleksandr Pasevin <pasevin@gmail.com>
2026-02-17 21:31:16 +0200

    chore(builder): update packages

--- apps/builder/src/export/versions.ts
+++ apps/builder/src/export/versions.ts
@@ -15,6 +15,6 @@ export const packageVersions = {
   '@openzeppelin/ui-storage': '1.0.0',
   '@openzeppelin/ui-types': '1.8.0',
   '@openzeppelin/ui-components': '1.2.1',
-  '@openzeppelin/ui-utils': '1.2.1',
+  '@openzeppelin/ui-utils': '1.2.2',
   '@openzeppelin/ui-styles': '1.0.0',
 };
